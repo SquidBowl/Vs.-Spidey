326abb6b1e16c08fc31d18a14062649dec954c16
Rosa <97431838+SquidBowl@users.noreply.github.com>
2022-10-18 05:51:06 -0400

    yeah

--- spidey/assets/preload/stages/pinkmall.lua
+++ spidey/assets/preload/stages/pinkmall.lua
@@ -1,10 +1,9 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('pnkbg', 'pnkbg', -700, -300);
-	setLuaSpriteScrollFactor('pnkbg', 0.9, 0.9);
-	
+	makeLuaSprite('pnkbg', 'pnkbg', -500, -600);
+	setScrollFactor('pnkbg', 1.0, 1.0);
 
 	addLuaSprite('pnkbg', false);
-
+	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
